a1d66ddeebdf8be51441542680a880ca93cadd1b
Ch0wW <Ch0wW@baseq.fr>
2016-02-15 23:39:09 +0100

    Added a batch template for Zandronum - Windows.
    Signed-off-by: Ch0wW <Ch0wW@baseq.fr>

---
+++ zandronum/templates/zandroserver.bat
@@ -0,0 +1,15 @@
+::===================
+::
+:: Windows Zandronum Server template.
+:: Don't forget to set all of your wads within that folder (IWAD included!)
+:: 
+:: If you want to add multiple PWADs:
+:: set PWAD="wad1.pk3 wad2.wad wad3.wad wad4.pk3"
+::
+::===================
+set PORT=10666
+set IWAD="DOOM2.WAD"
+set PWAD=""
+set CFG=""
+
+zandronum.exe -host -port %PORT% -iwad %IWAD% -file %PWAD% +exec %CFG% %ADD_PARMS%
